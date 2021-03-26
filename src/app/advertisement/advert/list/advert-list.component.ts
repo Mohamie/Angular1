5ef465733ac7e101f8b9e8a820e5a89dc338d85b
Nelson Mohamie <thabelo.590.nt@gmail.com>
2021-03-26 13:51:09 +0200

    Add - Search functionality

--- src/app/advertisement/advert/list/advert-list.component.ts
+++ src/app/advertisement/advert/list/advert-list.component.ts
@@ -9,6 +9,8 @@ import { AdvertService } from '../../data/services/advert.service';
 export class AdvertListComponent implements OnInit 
 {
   adverts: Advert[];
+  filteredAdverts: Advert[];
+  searchValue: string;
 
   constructor(private advertService: AdvertService) { }
 
@@ -18,12 +20,27 @@ export class AdvertListComponent implements OnInit
         {
           next: adverts =>
           {
-            this.adverts = adverts
+            this.adverts = adverts;
+            this.filteredAdverts = this.adverts;
+
           },
           error: err => console.error(err)
         }
       );
 
+  }
+
+  set filterAdvertsBySearch(value: string)
+  {
+    this.searchValue = value;
+    this.filteredAdverts = this.searchValue ? this.performFiltering(this.searchValue) : this.adverts;  
+  }
+
+  performFiltering(filterBy: string) : Advert[]
+  {
+      filterBy = filterBy.toLocaleLowerCase();
+
+      return this.adverts.filter((advert: Advert) => advert.title.toLocaleLowerCase().indexOf(filterBy) !== -1 || advert.description.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 
 }
